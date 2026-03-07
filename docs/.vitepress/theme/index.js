32aa6a9d50fdf27e92b5eb39619fe14a2f3a6c0f
CD010222 <1054079400@qq.com>
2026-03-07 10:53:51 +0800

    chinaEbus initial

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -3,20 +3,27 @@ import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 import { createPinia } from 'pinia'
-import PasswordGuard from './PasswordGuard.vue'
 
 /** @type {import('vitepress').Theme} */
+
+// import * as ElementPlusIconsVue from '@element-plus/icons-vue' // 全局注册 ElementPlusIconsVue
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
-      'layout-top': () => h(PasswordGuard, null, {
-        default: () => null
-      })
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
     })
   },
   enhanceApp({ app, router, siteData }) {
-    app.use(createPinia())
+    // use the plugin
+    app.use(createPinia()) // pinia
+
+    // // https://element-plus.org/zh-CN/component/icon.html
+    // for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    //   app.component(key, component)
+    // } // 全局注册 ElementPlusIconsVue
+
+
   }
 }
